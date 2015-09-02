1c8b4da360fbf22f2da312a47735f4e9efbe6c30
brandenbyers <branden@brandenbyers.com>
2015-09-01 22:47:07 -0500

    Add toggle for dual monitor workstations

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -56,6 +56,22 @@ end)
 --
 -- Workspaces
 --
+-- hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"},"F1", function()
+--   local macbook = "Color LCD"
+--   local thunderbolt = "Thunderbolt Display"
+--   local windowLayout = {
+--       {"Google Chrome", nil, thunderbolt, hs.layout.left50,  nil, nil},
+--       {"iTerm", nil, thunderbolt, hs.layout.right50, nil, nil},
+--       {"Messages", nil, thunderbolt, hs.layout.left30, nil, nil},
+--       {"Tweetbot", nil, thunderbolt, hs.layout.left30, nil, nil},
+--       {"Gitter", nil, thunderbolt, hs.layout.left30, nil, nil},
+--       {"nvALT", nil, thunderbolt, hs.layout.right25, nil, nil},
+--       {"PureCloud", nil, macbook, hs.layout.right50, nil, nil},
+--       {"Microsoft Outlook", nil, macbook, hs.layout.left50, nil, nil},
+--   }
+--   hs.layout.apply(windowLayout)
+-- end)
+
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"},"F2", function()
   local iMac = "iMac"
   local windowLayout = {
@@ -67,6 +83,85 @@ hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"},"F2", function()
       {"nvALT", nil, iMac, hs.layout.right25, nil, nil},
   }
   hs.layout.apply(windowLayout)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"},"F3", function()
+  local macbook = "Color LCD"
+  local windowLayout = {
+      {"Google Chrome", nil, macbook, hs.layout.left70,  nil, nil},
+      {"iTerm", nil, macbook, hs.layout.right30, nil, nil},
+      {"Messages", nil, macbook, hs.layout.right30, nil, nil},
+      {"Tweetbot", nil, macbook, hs.layout.right30, nil, nil},
+      {"Gitter", nil, macbook, hs.layout.right30, nil, nil},
+      {"nvALT", nil, macbook, hs.layout.right30, nil, nil},
+  }
+  hs.layout.apply(windowLayout)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "F1", function()
+  local macbook = "Color LCD"
+  local iMac = "iMac"
+  local thunderbolt = "Thunderbold Display"
+  if (hs.screen.allScreens()[1]:name() == macbook) then
+    if (hs.screen.allScreens()[2]) then
+      if (hs.screen.allScreens()[2]:name() == thunderbolt) then
+        -- dual display for work
+        local windowLayout = {
+            {"Google Chrome", nil, thunderbolt, hs.layout.left50,  nil, nil},
+            {"iTerm", nil, thunderbolt, hs.layout.right50, nil, nil},
+            {"Messages", nil, macbook, hs.layout.right30, nil, nil},
+            {"Tweetbot", nil, macbook, hs.layout.right30, nil, nil},
+            {"Gitter", nil, macbook, hs.layout.right30, nil, nil},
+            {"nvALT", nil, macbook, hs.layout.right30, nil, nil},
+        }
+        hs.layout.apply(windowLayout)
+      elseif (hs.screen.allScreens()[2]:name() == iMac) then
+        -- dual display for home
+        local windowLayout = {
+            {"Google Chrome", nil, iMac, hs.layout.left50,  nil, nil},
+            {"iTerm", nil, iMac, hs.layout.right50, nil, nil},
+            {"Messages", nil, macbook, hs.layout.right30, nil, nil},
+            {"Tweetbot", nil, macbook, hs.layout.right30, nil, nil},
+            {"Gitter", nil, macbook, hs.layout.right30, nil, nil},
+            {"nvALT", nil, macbook, hs.layout.right30, nil, nil},
+        }
+        hs.layout.apply(windowLayout)
+      end
+    else
+      -- macbook screen only, WORK
+      local windowLayout = {
+          {"Google Chrome", nil, macbook, hs.layout.left70,  nil, nil},
+          {"iTerm", nil, macbook, hs.layout.right30, nil, nil},
+          {"Messages", nil, macbook, hs.layout.right30, nil, nil},
+          {"Tweetbot", nil, macbook, hs.layout.right30, nil, nil},
+          {"Gitter", nil, macbook, hs.layout.right30, nil, nil},
+          {"nvALT", nil, macbook, hs.layout.right30, nil, nil},
+      }
+      hs.layout.apply(windowLayout)
+    end
+  elseif (hs.screen.allScreens()[1]:name() == thunderbolt) then
+    -- thunderbolt screen only
+    local windowLayout = {
+        {"Google Chrome", nil, thunderbolt, hs.layout.left50,  nil, nil},
+        {"iTerm", nil, thunderbolt, hs.layout.right50, nil, nil},
+        {"Messages", nil, thunderbolt, hs.layout.left25, nil, nil},
+        {"Tweetbot", nil, thunderbolt, hs.layout.left25, nil, nil},
+        {"Gitter", nil, thunderbolt, hs.layout.left25, nil, nil},
+        {"nvALT", nil, thunderbolt, hs.layout.left25, nil, nil},
+    }
+    hs.layout.apply(windowLayout)
+  elseif (hs.screen.allScreens()[1]:name() == iMac) then
+    -- iMac screen only, HOME
+    local windowLayout = {
+        {"Google Chrome", nil, iMac, hs.layout.left50,  nil, nil},
+        {"iTerm", nil, iMac, hs.layout.right50, nil, nil},
+        {"Messages", nil, iMac, hs.layout.left25, nil, nil},
+        {"Tweetbot", nil, iMac, hs.layout.left25, nil, nil},
+        {"Gitter", nil, iMac, hs.layout.left25, nil, nil},
+        {"nvALT", nil, iMac, hs.layout.left25, nil, nil},
+    }
+    hs.layout.apply(windowLayout)
+  end
 end)
 
 --
